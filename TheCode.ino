3f42785b25094e6c7f64da5ee9a5877816e747be
Timdeltrap <timdeltrap@hotmail.nl>
2018-10-16 12:09:39 +0200

    Update TheCode.ino

--- TheCode.ino
+++ TheCode.ino
@@ -86,3 +86,17 @@ void rpm()
  time1 = time2;
  time2 = micros();
 }
+
+void getPots() {
+  for(int i; i < 3; i++){
+    sensorValue[i] = analogRead(potPin[i]);
+    EMA_S[i] = (EMA_a*sensorValue[i]) + ((1-EMA_a)*EMA_S[i]);
+    if(EMA_S[i] != previousEMA_S[i]){
+      potValue1 = EMA_S[0];
+      potValue2 = EMA_S[1];
+      potValue3 = EMA_S[2];
+      previousEMA_S[i] = EMA_S[i];
+    }
+  }
+  
+}
